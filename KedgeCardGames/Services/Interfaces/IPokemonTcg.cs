51dda155c5cedc36b78026eed5f70cc81a98f1a9
IsaSamara <samara.santos3527@gmail.com>
2025-04-26 21:44:27 -0300

    Reapply "adicionado filtro"
    This reverts commit b27e5f6ecb992180801c629d8de03c33f1fbdc00.

--- KedgeCardGames/Services/Interfaces/IPokemonTcg.cs
+++ KedgeCardGames/Services/Interfaces/IPokemonTcg.cs
@@ -6,6 +6,9 @@ namespace KedgeCardGames.Services.Interfaces
     public interface IPokemonTcg
     {
         [Get("/v2/cards")]
-        Task<RespostaCartasPokemonDto> ObterCardsPokemon([AliasAs("page")] int page = 1, [AliasAs("pageSize")] int pageSize = 20);
+        Task<RespostaCartasPokemonDto> ObterCardsPokemon(
+            [AliasAs("page")] int page = 1, 
+            [AliasAs("pageSize")] int pageSize = 20,
+            [AliasAs("q")] string? query = null);
     }
 }
